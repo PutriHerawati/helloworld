d677b9c9287bf1bfdf4f5d91100dfbfba008dc35
PutriHerawati <contact.putriherawati@gmail.com>
2026-06-16 20:42:18 +0700

    Update text_encrypter.cpp

--- text_encrypter.cpp
+++ text_encrypter.cpp
@@ -9,8 +9,8 @@ int main() {
     int key = 3; // Shift key for encryption logic
 
     cout << "=== SECURE TEXT ENCRYPTER ===" << endl;
-    cout << "1. Encrypt Message (Amankan Pesan)\n";
-    cout << "2. Decrypt Message (Buka Pesan)\n";
+    cout << "1. Encrypt Message\n";
+    cout << "2. Decrypt Message\n";
     cout << "Select option (1-2): ";
     cin >> choice;
     cin.ignore(); // Clear the input buffer
